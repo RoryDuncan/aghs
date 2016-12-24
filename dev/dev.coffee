a8b029d7ebe89109f07aa547b115db8f583c94fa
Rory Duncan <robust.rory@gmail.com>
2016-12-24 19:02:02 +0000

    Update livedev structure to retain examples
    - Add keyboard example

--- dev/dev.coffee
+++ dev/dev.coffee
@@ -8,13 +8,37 @@ do () ->
   app = window.Aghs()
   world = app.world
   world.viewport(400, 400)
+  keyboard = app.keyboard
+  console.log(app)
   
-  app.render (time) ->
+  
+  # Examples
+  # examples namespace
+  examples = {}
+  
+  # move a square visually, by moving the world
+  # 
+  examples.movingSquareWithKeyboard = (time) ->
     app.clear("#fff")
-    world.move(-5, 0)
-    world.set(200, 0) if world.view.x * -1 > world.view.width
+    
+    world.move(0, 5)  if keyboard.keys.up
+    world.move(0, -5) if keyboard.keys.down
+    world.move(5, 0)  if keyboard.keys.left
+    world.move(-5, 0) if keyboard.keys.right
+    
+    
+    # wrap screen - if you reach the edge move back to the start, and vice-versa
+    # x's
+    world.set(0, world.view.y) if world.view.x * -1 > world.view.width
+    world.set(-world.view.width, world.view.y) if world.view.x * -1 < 0
+    # y's
+    world.set(world.view.x, 0) if world.view.y * -1 > world.view.height
+    world.set(world.view.x, -world.view.height) if world.view.y * -1 < 0
+    
     app.fillStyle "#ccc"
     world.fillRect(0, 0, 20, 20)
     world.debug()
-    
+  
+  # change the function below to view different examples
+  app.render(examples.movingSquareWithKeyboard)
   app.start()
--- dev/dev.js
+++ dev/dev.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, errorMessage, world;
+  var app, errorMessage, examples, keyboard, world;
   errorMessage = "index.js may be missing. Make sure to run 'npm run dev' to begin development.";
   if (!window.Aghs) {
     return console.alert(errorMessage);
@@ -7,15 +7,39 @@
   app = window.Aghs();
   world = app.world;
   world.viewport(400, 400);
-  app.render(function(time) {
+  keyboard = app.keyboard;
+  console.log(app);
+  examples = {};
+  examples.movingSquareWithKeyboard = function(time) {
     app.clear("#fff");
-    world.move(-5, 0);
+    if (keyboard.keys.up) {
+      world.move(0, 5);
+    }
+    if (keyboard.keys.down) {
+      world.move(0, -5);
+    }
+    if (keyboard.keys.left) {
+      world.move(5, 0);
+    }
+    if (keyboard.keys.right) {
+      world.move(-5, 0);
+    }
     if (world.view.x * -1 > world.view.width) {
-      world.set(200, 0);
+      world.set(0, world.view.y);
+    }
+    if (world.view.x * -1 < 0) {
+      world.set(-world.view.width, world.view.y);
+    }
+    if (world.view.y * -1 > world.view.height) {
+      world.set(world.view.x, 0);
+    }
+    if (world.view.y * -1 < 0) {
+      world.set(world.view.x, -world.view.height);
     }
     app.fillStyle("#ccc");
     world.fillRect(0, 0, 20, 20);
     return world.debug();
-  });
+  };
+  app.render(examples.movingSquareWithKeyboard);
   return app.start();
 })();
